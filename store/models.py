72f42cf2a7f26d8580d95df03019208d610c7141
griminir <viktor.elliot.king@gmail.com>
2024-11-11 14:20:27 +0100

    implemented a one to one relationship between custormer and address

--- store/models.py
+++ store/models.py
@@ -45,3 +45,8 @@ class Order(models.Model):
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(
         max_length=1, choices=PAYMENT_CHOICES, default=PAYMENT_PENDING)
+
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
