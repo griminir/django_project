c0a2b3f696464aaa79909d71a986ff61be646b18
griminir <viktor.elliot.king@gmail.com>
2024-11-15 10:02:26 +0100

    simple filtring practice

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product
 # Create your views here.
 
 
 def say_hello(request):
-    query_set = Product.objects.all()
-    
-    return render(request, 'hello.html', {'name': 'timmy'})
+    queryset = Product.objects.filter(inventory__lt=10)
+
+    return render(request, 'hello.html', {'name': 'timmy', 'products': list(queryset)})
