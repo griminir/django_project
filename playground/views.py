c55e3832fd09043303cd971f1826cb632e61b2fd
griminir <viktor.elliot.king@gmail.com>
2024-11-15 10:07:02 +0100

    complex lookup using Q objects

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q
 from store.models import Product
 # Create your views here.
 
 
 def say_hello(request):
-    queryset = Product.objects.filter(inventory__lt=10)
+    # complex lookup needs the Q object ~Q means not, (| or), (& and)
+    queryset = Product.objects.filter(Q(inventory__lt=10) | ~Q(unit_price__lt=20))
 
     return render(request, 'hello.html', {'name': 'timmy', 'products': list(queryset)})
