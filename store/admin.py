b3247824868ddc4bc95374b9723c517deaf6d108
griminir <viktor.elliot.king@gmail.com>
2024-11-21 11:28:48 +0100

    how to deal with related objects in admin page

--- store/admin.py
+++ store/admin.py
@@ -3,9 +3,13 @@ from . import models
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price', 'inventory_status']
+    list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
     list_editable = ['unit_price']
     list_per_page = 10
+    list_select_related = ['collection']
+
+    def collection_title(self, product):
+        return product.collection.title
 
     @admin.display(ordering='inventory')
     def inventory_status(self, product):
@@ -19,6 +23,10 @@ class CustomerAdmin(admin.ModelAdmin):
     list_editable = ['membership']
     ordering = ['first_name', 'last_name']
     list_per_page = 10
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'placed_at', 'customer']
 
 # Register your models here.
 # long way of doing it, add class and then pass it as a second argument
