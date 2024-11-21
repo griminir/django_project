74cfa91fa7ab296965cc2a21bb07d84d92566a93
griminir <viktor.elliot.king@gmail.com>
2024-11-21 12:42:12 +0100

    added search to customers

--- store/admin.py
+++ store/admin.py
@@ -27,19 +27,21 @@ class ProductAdmin(admin.ModelAdmin):
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name', 'membership', 'orders_count']
+    list_display = ['first_name', 'last_name', 'membership', 'orders']
     list_editable = ['membership']
-    ordering = ['first_name', 'last_name']
     list_per_page = 10
+    ordering = ['first_name', 'last_name']
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
 
-    def orders_count(self, customer):
+    def orders(self, customer):
         url = (reverse('admin:store_order_changelist')
-               + '?'
-               + urlencode({'customer__id': str(customer.id)}))
-        return format_html('<a href="{}">{}</a>', url, customer.orders_count)
+                + '?'
+                + urlencode({'customer__id': str(customer.id)}))
+        return format_html('<a href="{}">{} orders</a>', url, customer.orders)
 
     def get_queryset(self, request: HttpRequest) -> QuerySet:
-        return super().get_queryset(request).annotate(orders_count=Count('order'))
+        return super().get_queryset(request).annotate(orders=Count('order'))
+
 
 @admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
@@ -56,8 +58,8 @@ class CollectionAdmin(admin.ModelAdmin):
     @admin.display(ordering='products_count')
     def products_count(self, collection):
         url = (reverse('admin:store_product_changelist')
-               + '?'
-               + urlencode({'collection__id': str(collection.id)}))
+                + '?'
+                + urlencode({'collection__id': str(collection.id)}))
         return format_html('<a href="{}">{}</a>', url, collection.products_count)
 
     def get_queryset(self, request: HttpRequest) -> QuerySet:
