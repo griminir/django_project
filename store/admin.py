b7195988f248f28202dd9d798436ac337e466145
griminir <viktor.elliot.king@gmail.com>
2024-11-21 11:43:36 +0100

    override the queryset to get amount of products in each category

--- store/admin.py
+++ store/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.db.models.query import QuerySet
+from django.db.models import Count
+from django.http import HttpRequest
 from . import models
 
 @admin.register(models.Product)
@@ -30,4 +33,14 @@ class OrderAdmin(admin.ModelAdmin):
 
 # Register your models here.
 # long way of doing it, add class and then pass it as a second argument
-admin.site.register(models.Collection)
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['title', 'products_count']
+
+    @admin.display(ordering='products_count')
+    def products_count(self, collection):
+        return collection.products_count
+    
+    def get_queryset(self, request: HttpRequest) -> QuerySet:
+        return super().get_queryset(request).annotate(products_count=Count('product'))
+
