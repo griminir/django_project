599b1ab422fe43ac16323326fd4ba7a2fbd88614
griminir <viktor.elliot.king@gmail.com>
2024-11-21 12:03:12 +0100

    added how to redirect using django

--- store/admin.py
+++ store/admin.py
@@ -2,11 +2,15 @@ from django.contrib import admin
 from django.db.models.query import QuerySet
 from django.db.models import Count
 from django.http import HttpRequest
+from django.utils.html import format_html, urlencode
+from django.urls import reverse
 from . import models
+
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
+    list_display = ['title', 'unit_price',
+                    'inventory_status', 'collection_title']
     list_editable = ['unit_price']
     list_per_page = 10
     list_select_related = ['collection']
@@ -20,12 +24,22 @@ class ProductAdmin(admin.ModelAdmin):
             return 'Low'
         return 'OK'
 
+
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name', 'membership']
+    list_display = ['first_name', 'last_name', 'membership', 'orders_count']
     list_editable = ['membership']
     ordering = ['first_name', 'last_name']
     list_per_page = 10
+
+    def orders_count(self, customer):
+        url = (reverse('admin:store_order_changelist')
+               + '?'
+               + urlencode({'customer__id': str(customer.id)}))
+        return format_html('<a href="{}">{}</a>', url, customer.orders_count)
+
+    def get_queryset(self, request: HttpRequest) -> QuerySet:
+        return super().get_queryset(request).annotate(orders_count=Count('order'))
 
 @admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
@@ -33,14 +47,18 @@ class OrderAdmin(admin.ModelAdmin):
 
 # Register your models here.
 # long way of doing it, add class and then pass it as a second argument
+
+
 @admin.register(models.Collection)
 class CollectionAdmin(admin.ModelAdmin):
     list_display = ['title', 'products_count']
 
     @admin.display(ordering='products_count')
     def products_count(self, collection):
-        return collection.products_count
-    
+        url = (reverse('admin:store_product_changelist')
+               + '?'
+               + urlencode({'collection__id': str(collection.id)}))
+        return format_html('<a href="{}">{}</a>', url, collection.products_count)
+
     def get_queryset(self, request: HttpRequest) -> QuerySet:
         return super().get_queryset(request).annotate(products_count=Count('product'))
-
