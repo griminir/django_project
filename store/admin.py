2eed15b73ae9a9e0cf6f911ce515e1820753f205
griminir <viktor.elliot.king@gmail.com>
2024-11-21 11:11:22 +0100

    making it customizable and using the shorter syntax

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 from . import models
 
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'unit_price']
+    list_editable = ['unit_price']
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership']
+    list_editable = ['membership']
+    ordering = ['first_name', 'last_name']
+    list_per_page = 10
+
 # Register your models here.
+# long way of doing it, add class and then pass it as a second argument
 admin.site.register(models.Collection)
-admin.site.register(models.Product)
--- store/models.py
+++ store/models.py
@@ -52,6 +52,7 @@ class Customer(models.Model):
     birth_date = models.DateField(null=True)
     membership = models.CharField(
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+    
 
 
 class Order(models.Model):
